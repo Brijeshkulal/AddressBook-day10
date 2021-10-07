66cc5b54ea3bea4c220b05e77bcbab39a99a6920
“Brijesh” <brijeshkulal5@gmail.com>
2021-10-07 20:22:53 +0530

    Created a contact

--- com/labz/addressbook/AdressBook.java
+++ com/labz/addressbook/AdressBook.java
@@ -3,6 +3,22 @@ package com.labz.addressbook;
 public class AdressBook {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book");
+		String firstName = "Brijesh";
+		String lastName = "kulal";
+		String address = "Near SM Temple Badaje";
+		String city = "Manjeshwar";
+		String state = "Kerala";
+		int pinCode = 671323;
+		long mobileNumber = 8899221100l;
+		String emailId = "Raj123@gmail.com";
+		System.out.println("Name 	     : "  + firstName + " " + lastName + "\n"
+                + "Address      : "  + address + "\n"
+                + "City         : "  + city + "\n"
+                + "State        : "  + state + "\n"
+                + "ZipCode      : "  + pinCode + "\n"
+                + "MobileNumber : "  + mobileNumber + "\n"
+                + "EmailId      : "  + emailId + "\n" );
+
 	}
 
 }
